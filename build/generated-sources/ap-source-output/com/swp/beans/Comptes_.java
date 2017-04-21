e670bf01002627e16cecbeefa2afe7e27b84f9ba
wilofice <wilofice@gmail.com>
2017-04-21 07:54:14 +0100

    first commit

---
+++ build/generated-sources/ap-source-output/com/swp/beans/Comptes_.java
@@ -0,0 +1,17 @@
+package com.swp.beans;
+
+import com.swp.beans.Enseignant;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T07:51:45")
+@StaticMetamodel(Comptes.class)
+public class Comptes_ { 
+
+    public static volatile SingularAttribute<Comptes, String> idUser;
+    public static volatile ListAttribute<Comptes, Enseignant> enseignantList;
+    public static volatile SingularAttribute<Comptes, String> motdepasse;
+
+}
---
+++ build/generated-sources/ap-source-output/com/swp/beans/Semestre_.java
@@ -0,0 +1,17 @@
+package com.swp.beans;
+
+import com.swp.beans.Semaine;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T07:51:45")
+@StaticMetamodel(Semestre.class)
+public class Semestre_ { 
+
+    public static volatile SingularAttribute<Semestre, Integer> idsemestre;
+    public static volatile ListAttribute<Semestre, Semaine> semaineList;
+    public static volatile SingularAttribute<Semestre, String> semainedebut;
+
+}
